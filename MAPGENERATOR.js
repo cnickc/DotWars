75b36f133980726ea1cea8ea9358974ae32abaa2
cnickc <camshaft75@hotmail.com>
2015-01-20 15:47:06 -0500

    new 4 player map

--- MAPGENERATOR.js
+++ MAPGENERATOR.js
@@ -31,7 +31,7 @@ MapGenerator.prototype = {
 
 	FourPlayerMap : function () {
 		var b = [];		
-		var rand = Math.floor( Math.random()*3 );
+		var rand = Math.floor( Math.random()*4 );
 		
 		if( rand == 0 ) {
 			b.push( new Base( 40, 40, 30, 0 ) );
@@ -67,6 +67,23 @@ MapGenerator.prototype = {
 			b.push( new Base( 250, 450, 20, 14 ) );
 			b.push( new Base( 50, 250, 20, 15 ) );
 			b.push( new Base( 450, 250, 20, 16 ) );
+		} else if ( rand == 3 ) {
+			b.push( new Base( 40, 40, 30, 0 ) );
+			b.push( new Base( 460, 40, 30, 1 ) );
+			b.push( new Base( 40, 460, 30, 2 ) );
+			b.push( new Base( 460, 460, 30, 3 ) );
+			b.push( new Base( 100, 40, 20, 4 ) );
+			b.push( new Base( 40, 100, 20, 5 ) );
+			b.push( new Base( 100, 100, 20, 6 ) );
+			b.push( new Base( 400, 460, 20, 7 ) );
+			b.push( new Base( 460, 400, 20, 8 ) );
+			b.push( new Base( 400, 400, 20, 9 ) );
+			b.push( new Base( 400, 40, 20, 10 ) );
+			b.push( new Base( 460, 100, 20, 11 ) );
+			b.push( new Base( 400, 100, 20, 12 ) );
+			b.push( new Base( 100, 460, 20, 13 ) );
+			b.push( new Base( 40, 400, 20, 14 ) );
+			b.push( new Base( 100, 400, 20, 15 ) );
 		}
 		return b;
 	},
